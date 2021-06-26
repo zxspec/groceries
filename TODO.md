e7aced11fd435d9df2dd2dab53e6e4331c9dae3c
Dmytro Iievliev <ievlevdmitriy@gmail.com>
2021-06-26 22:21:57 +0200

    added "/api/search" route

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
 1. use [fork-ts-checker-webpack-plugin](https://github.com/TypeStrong/fork-ts-checker-webpack-plugin) to optimise build performance
 2. ~~copy `.env` file to `./dist` folder~~
+3. move `.env` files to `./settings` folder
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import fakeGroceriesData from "./mock/groceries.json";
 
 const ENV_PATH = path.resolve(__dirname, ".env");
 console.log("### ENV_PATH: ", ENV_PATH);
@@ -8,6 +9,13 @@ dotenv.config({ path: ENV_PATH });
 
 const PORT = process.env.PORT || 9999;
 const app = express();
+
+app.get("/api/search", (req: Request, res: Response) => {
+  const content = fakeGroceriesData;
+
+  res.set("content-type", "application/json");
+  res.send(content);
+});
 
 app.get("*", (_: Request, res: Response) => {
   const content = "Hello from Groceries Search!!!";
