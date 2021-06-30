9adf57be99167bb2a5f04eaf2dbb612dd28d43b6
Dmytro Iievliev <ievlevdmitriy@gmail.com>
2021-06-30 22:37:55 +0200

    added product search requests for SearchBox

--- src/client/components/SearchBox.tsx
+++ src/client/components/SearchBox.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
+import { fetchProducts } from "../store/actions";
 import "./SearchBox.css";
 
-export const SearchBox = () => (
-  <div className="searchbox">
-    <input type="text" name="search" placeholder="Type something in" />
-    <button>Search</button>
-  </div>
-);
+export const SearchBox = () => {
+  const dispatch = useDispatch();
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(fetchProducts(e.target.value));
+  };
+
+  return (
+    <div className="searchbox">
+      <input
+        type="text"
+        name="search"
+        placeholder="Type something in"
+        onChange={onChangeHandler}
+      />
+      <button>Search</button>
+    </div>
+  );
+};
