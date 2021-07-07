07a581059f76c8580602fe203c37e4ac813f39e3
Dmytro Iievliev <ievlevdmitriy@gmail.com>
2021-07-07 23:51:00 +0200

    added js-minification in production mode

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -1,10 +1,18 @@
 const minimist = require("minimist");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const argv = minimist(process.argv.slice(2));
 const isDev = !argv.mode || argv.mode === "development";
 const isProd = !isDev;
+
+const optimization = isProd
+  ? {
+      minimize: true,
+      minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    }
+  : {};
 
 module.exports = {
   mode: isDev ? "development" : "production",
@@ -23,9 +31,7 @@ module.exports = {
       },
     ],
   },
-  optimization: {
-    minimizer: isProd ? [new CssMinimizerPlugin()] : [],
-  },
+  optimization,
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
